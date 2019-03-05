12cdf6047943be0eac91214274e425854641a5f7
Duncan <duncmc831@gmail.com>
2019-03-05 14:55:56 -0800

    added: endpoints for data set and cluster solution

--- cluster/api/dataset.py
+++ cluster/api/dataset.py
@@ -1,29 +1,39 @@
-
-# api/dataset.py
-
-from flask import current_app, Response
 from flask_restplus import fields, Resource
-from cluster.api.restplus import api, mimetype
-from cluster.database_update.dataset_table import dataset as table
-
-table_name = 'dataset'
+from cluster.api.restplus import api
+from cluster.database.tableaccess import engine, dataset, alldatasets, cluster_solution, cluster_solutions_per_dataset
+table_name = 'datasets'
 ns = api.namespace('dataset')
 model = api.model('dataset', {
     'name': fields.String(required=True, description='Unique dataset name'),
-    'uuid': fields.String(description='Universally unique ID'),
-    'species': fields.String(description='Species studied'),
-    'organ': fields.String(description='source organ'),
-    'cell_count': fields.Integer(description='Count of cells in the dataset'),
-    'disease': fields.String(description='Any disease of the dataset, i.e. cancer'),
-    'platform': fields.String(description='Genomic sequencing platform'),
-    'data_source_url': fields.String(description='URL of data source'),
-    'publication_url': fields.String(description='URL of publication'),
+    'description': fields.String(description='Unique dataset name'),
+    'id': fields.String(description='Access identifier for the data set')
 })
 
-# Get all
+cs_model = api.model('cluster_solution', {
+    'name': fields.String(required=True, description='Curator made identifier'),
+    'method': fields.String(description='Curator method identifier'),
+    "id": fields.String(description='Access identifier for the cluster solution')}
+                     )
+
 @ns.route('/')
-class List(Resource):
-    @ns.response(200, 'list of all ' + table_name + 's in TSV format')
+class DatasetList(Resource):
+    @api.marshal_with(model, envelope="resource")
+    @ns.response(200, 'list of all datasets, with name, description, and id')
     def get(self):
-        '''GET ALL'''
-        return Response(table.get_all(), mimetype=mimetype)
+        """a list of data sets: name, description, and id."""
+        return alldatasets(dataset, engine.connect())
+
+@ns.route('/<int:id>/cluster-solutions')
+@ns.param('id', 'Data set identifier')
+class ClusterSolsForDataset(Resource):
+    @api.marshal_with(cs_model, envelope="resource")
+    @ns.response(200, 'list of available clustering solutions for a dataset')
+    def get(self, id):
+        """a list of available cluster solutions for a data set: name, method and id."""
+
+        return cluster_solutions_per_dataset(
+            cluster_solution,
+            dataset,
+            id,
+            engine.connect()
+        )
