e1f5708fdf91342f57bc0d1cfe2c1bd1517e3acd
Duncan <duncmc831@gmail.com>
2019-03-05 14:56:26 -0800

    added: endpoint for cell assignments

--- cluster/api/cluster_solution.py
+++ cluster/api/cluster_solution.py
@@ -1,21 +1,28 @@
 
 # api/cluster_solution.py
 
-from flask import request, Response
-from flask_restplus import fields,  Resource
-from cluster.api.restplus import api, mimetype
-
+from flask_restplus import fields, Resource
+from cluster.api.restplus import api
+from cluster.database.tableaccess import cellassignments, engine, cluster_assignment, cluster, cluster_solution
 table_name = 'cluster_solution'
 ns = api.namespace('cluster-solution')
-model = api.model('cluster_solution', {
-    'name': fields.String(required=True, description='Unique cluster solution name'),
-    'method': fields.String(required=True, description='Clustering method applied'),
-    'method_implementation': fields.String(required=True, description='Clustering method implementation'),
-    'method_url': fields.String(required=True, description='URL of clustering method'),
-    'method_parameters': fields.String(required=True, description='Clustering method parameters'),
-    'scores': fields.String(description='Scores for this cluster solution'),
-    'analyst': fields.String(required=True, description='Person who ran the analysis'),
-    'secondary': fields.Integer(required=True, \
-        description='One means this is a secondary cluster solution and some other cluster solution is the default'),
-    'dataset': fields.String(required=True, description='Name of dataset that was analyzed'),
-})
+ca_model = api.model('cluster-assignment', {
+    'name': fields.String(required=True, description='identifier for the cell (within a data set)'),
+    'cluster_name': fields.String(description='string identifier for the cluster')
+    }
+)
+@ns.route('/<int:id>/cell-assignments')
+@ns.param('id', 'Cluster solution identifier')
+class CellAssignment(Resource):
+    @api.marshal_with(ca_model, envelope="resource")
+    @ns.response(200, '')
+    def get(self, id):
+        """cluster assignments of cells in a clustering solution."""
+
+        return cellassignments(
+            cluster_assignment,
+            cluster,
+            cluster_solution,
+            id,
+            engine.connect()
+        )
