cbf037232946f635b2b44325a1c61f4269d49654
Duncan <duncmc831@gmail.com>
2019-03-28 11:15:55 -0700

    added: ORM definitions for tables

---
+++ cluster/database/models.py
@@ -0,0 +1,109 @@
+"""
+Provides access to database through sqlalchemy core objects (sqlalchemy.sql.schema.Table) and SQL alchemy ORMs
+"""
+from cluster.database import db
+from cluster.database.access import engine
+from sqlalchemy import Table, MetaData, Column, Integer, String, Float, ForeignKey, Float
+
+# Connection to the database.
+metadata = MetaData()
+
+
+dataset = Table('dataset', metadata, autoload=True, autoload_with=engine)
+
+
+cluster_solution = Table('cluster_solution', metadata, autoload=True, autoload_with=engine)
+
+
+cluster = Table('cluster', metadata, autoload=True, autoload_with=engine)
+
+
+cell_assignment = Table('cell_of_cluster', metadata, autoload=True, autoload_with=engine)
+
+
+class Dataset(db.Model):
+
+    __tablename__ = "dataset"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False, unique=True)
+    uuid = Column(String)
+    species = Column(String)
+    organ = Column(String)
+    cell_count = Column(Integer)
+    disease = Column(String)
+    platform = Column(String)
+    description = Column(String)
+    data_source_url = Column(String)
+    publication_url = Column(String)
+
+    def __repr__(self):
+     return "<User(name=%s, species=%s, organ=%s, cell count=%d, data source url=%s )>" % \
+            (self.name, self.species, self.organ, self.cell_count, self.data_source_url)
+
+
+class ClusterSolution(db.Model):
+
+    __tablename__ = "cluster_solution"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    description = Column(String)
+    method = Column(String)
+    method_implementation = Column(String)
+    method_url = Column(String)
+    method_parameters = Column(String)
+    scores = Column(String)
+    analyst  = Column(String)
+    likes = Column(Integer)
+    expression_hash = Column(String)
+    dataset_id = Column(Integer, ForeignKey("dataset.id"), nullable=False)
+
+
+class Cluster(db.Model):
+
+    __tablename__ = "cluster"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    label = Column(String)
+    description = Column(String)
+    cluster_solution_id = Column(Integer, ForeignKey("cluster_solution.id"), nullable=False)
+
+
+class CellAssignment(db.Model):
+
+    __tablename__ = "cell_of_cluster"
+
+    id = Column(String, primary_key=True, autoincrement=True)
+    name = Column(String, nullable=False)
+    cluster_id = Column(Integer, ForeignKey("cluster.id"), nullable=False)
+
+
+class Marker(db.Model):
+
+    __tablename__ = "marker"
+
+    id = Column(Integer, primary_key=True)
+    hugo_name = Column(String)
+    ensembl_name = Column(String)
+    sensitivity = Column(Float)
+    specificity = Column(Float)
+    precision = Column(Float)
+    recall = Column(Float)
+    accuracy = Column(Float)
+    t_pval = Column(Float)
+    z_pval = Column(Float)
+    t_stat = Column(Float)
+    z_stat = Column(Float)
+    log2_fold_change_vs_min = Column(Float)
+    log2_fold_change_vs_next = Column(Float)
+    mean_expression = Column(Float)
+    dataset_id = Column(Integer, ForeignKey("dataset.id"), nullable=False)
+    cluster_solution_id = Column(Integer, ForeignKey("cluster_solution.id"), nullable=False)
+    cluster_id = Column(Integer, ForeignKey("cluster.id"), nullable=False)
+
+    def __repr__(self):
+
+        return("<Marker(name=%s, tstat=%f, mean=%f, log2fc_next=%f)>" %
+               (self.hugo_name, self.t_stat, self.mean_expression, self.log2_fold_change_vs_next))
