6d9b4a3883105bcc9d1f678fd896c434a37735d1
“terraswat” <t@santacruzwood.com>
2019-05-23 13:53:03 -0700

    added back swagger content-type option

--- cluster/api/restplus.py
+++ cluster/api/restplus.py
@@ -5,9 +5,19 @@ log = logging.getLogger(__name__)
 api = Api(version='0.1.0', title='Cluster Database API')
 mimetype = 'text/plain'
 
+
 # Default error handler.
 @api.errorhandler
 def default_error_handler(e):
     message = 'Error: ' + str(e)
     log.exception(message)
     return {'message': message}, 500
+
+
+# Only needed to put the 'text-tsv' response content-type
+# option in the drop-down on the swagger page.
+@api.representation('text-tsv')
+def tsv_response(data, code, headers=None):
+       resp = app.make_response(data)
+       resp.headers['Content-Type'] = 'application/json'
+       return resp
