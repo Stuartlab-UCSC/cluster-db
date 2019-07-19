c920972cd155fa0c75fc2121b3014c8c092670f8
Duncan <duncmc831@gmail.com>
2019-07-19 09:07:48 -0700

    refactor: globals of api.users moved into default entries

--- cluster/database/default_entries.py
+++ cluster/database/default_entries.py
@@ -1,9 +1,22 @@
 
 
-from cluster.api.user import TEST_STATE_PATH, TEST_MARKERS_DICT_PATH, TEST_CLUSTER_ID_PATH, TEST_XY_PATH, TEST_EXPRESSION_PICKLE_PATH, TEST_CLUSTER_TABLE_PATH
+
 from cluster.database.user_models import  UserExpression, ClusterGeneTable, ExpDimReduct, ExpCluster, CellTypeWorksheet, WorksheetUser
 
+# These are all the global variables that will need access functions once the data serves more than worksheet.
+TEST_STATE_PATH = "../users/test/test_state.json.gzip"
+TEST_CLUSTER_TABLE_PATH = "../users/test/clusters_table.tab"
+TEST_MARKERS_DICT_PATH = "../users/test/krigstien6000k_markers.json.gzip"
+TEST_EXPRESSION_PICKLE_PATH = "../users/test/exp.pi"
+TEST_CLUSTER_ID_PATH = "../users/test/louvain:res:1.50.pi"
+TEST_XY_PATH = "../users/test/X_umap.pi"
+DEFAULT_CLUSTER_SOLUTION="1"
+DEFAULT_SCATTER_TYPE="umap"
+# The cluster solution name matches what is int the cluster-id file and a key inside the markers.json.gzip
+CLUSTER_SOLUTION_NAME = "louvain:res:1.50"
 
+#You were getting the paths into the user directory correct...
+# entries are defined as a tuple of dictionaries mapping to keys and the db.Model class
 entries = []
 
 
