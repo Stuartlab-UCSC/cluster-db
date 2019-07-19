d0306b72eb0f252819db0544f76577bcba0b6c14
Duncan <duncmc831@gmail.com>
2019-07-19 09:06:21 -0700

    add: utility functions for accessing worksheet tables

--- cluster/database/user_models.py
+++ cluster/database/user_models.py
@@ -1,6 +1,7 @@
 from cluster.database import Model, SurrogatePK, backref, relationship
 from flask_user import UserMixin
 from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, DateTime
+from cluster.database.filename_constants import XYS, EXPRESSION, CLUSTERING, MARKER_TABLE, STATE
 
 
 class Role(Model):
@@ -123,3 +124,111 @@ class ClusterGeneTable(SurrogatePK, Model):
     def get_table(cls, cluster):
         return cls.query.filter(cls.cluster_id == cluster.id).first()
 
+
+def get_all_worksheet_paths(user_email, worksheet_name):
+    user = User.get_by_email(user_email)
+    ws = WorksheetUser.get_worksheet(user, worksheet_name)
+    user_exp = UserExpression.get_by_worksheet(ws)
+    clustering = ExpCluster.get_cluster(user_exp) # assumes ther is only one...
+    marker_table = ClusterGeneTable.get_table(clustering)
+    reduction = ExpDimReduct.get_by_expression(user_exp) #assumes there is only one ...
+    return {
+        XYS: reduction.place,
+        STATE: ws.place,
+        EXPRESSION: user_exp.place,
+        CLUSTERING: clustering.place,
+        MARKER_TABLE: marker_table.place,
+    }
+
+
+def add_worksheet_entries(
+        session,
+        user_email,
+        worksheet_name,
+        organ=None,
+        species=None,
+        dataset_name=None,
+        cluster_name=None,
+):
+    """
+    Single api for the tables needed to have a worksheet.
+    :param session:
+    :param worksheet_name:
+    :param worksheet_path:
+    :param user_id:
+    :param exp_data_path:
+    :param cluster_path:
+    :param reductions:
+    :param organ:
+    :param species:
+    :param dataset_name:
+    :param cluster_name:
+    :return:
+    """
+    try:
+        get_all_worksheet_paths(user_email, worksheet_name)
+    except: # get all throws an error when there isn't an entry
+        print("akjsdhfkladskfakdsf adding entry")
+        import os
+        worksheet_root = os.path.join(user_email, worksheet_name)
+        worksheet_path = os.path.join(worksheet_root, STATE)
+        exp_data_path  = os.path.join(worksheet_root, EXPRESSION)
+        cluster_path = os.path.join(worksheet_root, CLUSTERING)
+        xys_path = os.path.join(worksheet_root, XYS)
+        marker_path = os.path.join(worksheet_root, MARKER_TABLE)
+
+        user_id = User.get_by_email(user_email).id
+
+        ws = CellTypeWorksheet(
+            name=worksheet_name,
+            place=worksheet_path
+        )
+
+        session.add(ws)
+        session.commit()
+
+        ws_user = WorksheetUser(
+            user_id = user_id,
+            worksheet_id = ws.id
+        )
+
+        session.add(ws_user)
+        session.commit()
+
+        user_exp = UserExpression(
+            species=species,
+            organ=organ,
+            name=dataset_name,
+            place=exp_data_path,
+            worksheet_id=ws.id
+        )
+        session.add(user_exp)
+        session.commit()
+
+
+        reduct = ExpDimReduct(
+            name="xys",
+            expression_id=user_exp.id,
+            place=xys_path
+        )
+        session.add(reduct)
+        session.commit()
+
+
+        cluster = ExpCluster(
+            name=cluster_name,
+            expression_id=user_exp.id,
+            place=cluster_path
+        )
+
+        session.add(cluster)
+        session.commit()
+
+
+        marker_table = ClusterGeneTable(
+            place=marker_path,
+            cluster_id=cluster.id
+        )
+
+        session.add(marker_table)
+        session.commit()
