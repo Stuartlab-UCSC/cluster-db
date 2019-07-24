62f9e4bb5c39790c6850b06106cfd78b3eec11f2
Duncan <duncmc831@gmail.com>
2019-07-23 18:27:59 -0700

    remove: duplicated .config vars

--- cluster/settings.py
+++ cluster/settings.py
@@ -9,8 +9,8 @@ def directory_back_from_this_file():
 # Used for default location of database
 CLUSTERDB = os.path.join(directory_back_from_this_file(), "cluster.db")
 USERDB = os.path.join(directory_back_from_this_file(), "cluster_user.db")
-VIEWER_URL= os.environ.get("VIEWER_URL", "localhost:3000/")
-USER_DIRECTORY= os.environ.get("USER_DIRECTORY")
+VIEWER_URL = os.environ.get("VIEWER_URL", "localhost:3000/")
+USER_DIRECTORY = os.environ.get("USER_DIRECTORY")
 # Flask settings
 FLASK_DEBUG = os.environ.get('FLASK_DEBUG')
 #SERVER_NAME = os.environ.get('FLASK_SERVER_NAME')
@@ -25,26 +25,6 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 RESTPLUS_SWAGGER_UI_DOC_EXPANSION = 'list'
 RESTPLUS_VALIDATE = True
 RESTPLUS_MASK_SWAGGER = False
-
-# Flask-Mail SMTP server settings
-MAIL_SERVER = 'smtp.com'
-MAIL_PORT = 465
-MAIL_USE_SSL = True
-MAIL_USE_TLS = False
-MAIL_USERNAME = 'hexmap@ucsc.edu'
-MAIL_PASSWORD = 'password'
-MAIL_DEFAULT_SENDER = '"UCSC Cell Atlas" <hexmap@ucsc.edu>'
-
-# Features
-USER_ENABLE_USERNAME = False # email auth only, no username is used
-
-# Generic
-USER_APP_NAME = "UCSC Cell Atlas"
-USER_AUTO_LOGIN = False
-USER_AUTO_LOGIN_AFTER_REGISTER = False
-USER_AUTO_LOGIN_AT_LOGIN = False
-USER_EMAIL_SENDER_NAME = USER_APP_NAME
-USER_EMAIL_SENDER_EMAIL = '"UCSC Cell Atlas" <hexmap@ucsc.edu>'
 
 # FLASK-USER CONFIG:
 # https://flask-user.readthedocs.io/en/latest/configuring_settings.html
@@ -72,11 +52,9 @@ USER_AUTO_LOGIN_AT_LOGIN = False
 USER_EMAIL_SENDER_NAME = USER_APP_NAME
 USER_EMAIL_SENDER_EMAIL = '"UCSC Cell Atlas" <hexmap@ucsc.edu>'
 
-# URLs
 USER_EDIT_USER_PROFILE_URL = '/user/change-password'
 
 # Endpoints
-
 USER_AFTER_CHANGE_PASSWORD_ENDPOINT = 'home'
 USER_AFTER_CONFIRM_ENDPOINT = 'user.login'
 USER_AFTER_FORGOT_PASSWORD_ENDPOINT = 'home'
