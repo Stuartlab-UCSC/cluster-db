a05e6814c20a78e632651209f4e026f0defb64cd
Duncan <duncmc831@gmail.com>
2019-08-13 10:48:19 -0700

    add: constant for cluster to filename dict

--- cluster/database/filename_constants.py
+++ cluster/database/filename_constants.py
@@ -4,5 +4,5 @@ EXPRESSION = "exp.pi"
 XYS = "xys.pi"
 MARKER_TABLE = "markers.pi"
 STATE = "state.json.gz"
-
+SPLIT_CLUSTER_DICT = "cluster_dict.pi"
 fname_keys = [CLUSTERING, EXPRESSION, XYS, MARKER_TABLE, STATE]
