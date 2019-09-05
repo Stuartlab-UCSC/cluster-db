f582ce01ec39d169b0e3a6d3619d87d496226131
Duncan <duncmc831@gmail.com>
2019-09-04 20:02:17 -0700

    add: cell type annotation filename

--- cluster/database/filename_constants.py
+++ cluster/database/filename_constants.py
@@ -5,4 +5,5 @@ XYS = "xys.pi"
 MARKER_TABLE = "markers.pi"
 STATE = "state.json.gz"
 SPLIT_CLUSTER_DICT = "cluster_dict.pi"
-fname_keys = [CLUSTERING, EXPRESSION, XYS, MARKER_TABLE, STATE]
+CELL_TYPE_ANNOTATION = "celltype_annotate.pi"
+fname_keys = [CLUSTERING, EXPRESSION, XYS, MARKER_TABLE, STATE, CELL_TYPE_ANNOTATION]
